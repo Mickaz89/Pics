2ca64684d7701021ab567d357cb825347642e1b1
Mickael <zanamickael@gmail.com>
2019-09-23 21:06:28 +0300

    Adding CSS styles & created a function to get image's height

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'https:\//api.unsplash.com' ,
+	baseURL: 'https://api.unsplash.com' ,
 	headers: {
-		Authorization: 'Client-ID a5c66fd4f634824c08a974470f857d78c1fb70d29258da6bf0e29f6c05b774ba' 
+		Authorization: 'Client-ID a5c66fd4f634824c08a974470f857d78c1fb70d29258da6bf0e29f6c05b774ba'
 	}
 });
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import './ImageList.css';
+import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 	const images = props.images.map((image) => {
-		return <img key = {image.id} src={image.urls.regular} />
+		return <ImageCard key={image.id} image={image} />
 	});
 
 	return(
 
-		<div> {images} </div>
+		<div className="image-list"> {images} </div>
 
 		);
 }
